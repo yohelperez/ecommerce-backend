7c846d971dac3df6491407efc4d56600f0d7bafa
yohelperez <perezyohel@gmail.com>
2024-11-03 20:50:48 -0500

    add CORS setting and admin account creation

--- src/main/java/com/example/ecommerce/ecommerce/services/auth/AuthServiceImpl.java
+++ src/main/java/com/example/ecommerce/ecommerce/services/auth/AuthServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.ecommerce.ecommerce.dto.UserDto;
 import com.example.ecommerce.ecommerce.entity.User;
 import com.example.ecommerce.ecommerce.enums.UserRole;
 import com.example.ecommerce.ecommerce.respository.UserRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -33,5 +34,18 @@ public class AuthServiceImpl implements AuthService {
 
     public Boolean hasUserWithEmail(String email) {
         return userRepository.findFirstByEmail(email).isPresent();
+    }
+
+    @PostConstruct
+    public void createAdminAccount() {
+        User adminAccount = userRepository.findByRole(UserRole.ADMIN);
+        if (adminAccount == null) {
+            User user = new User();
+            user.setEmail("admin@test.com");
+            user.setName("admin");
+            user.setRole(UserRole.ADMIN);
+            user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+            userRepository.save(user);
+        }
     }
 }
